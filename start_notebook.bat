38572bfe1ca9164a47d321a65925adebb746b54a
mishagrol <grol81@mail.ru>
2021-03-05 22:30:40 +0300

    add: License

--- start_notebook.bat
+++
@@ -1,3 +0,0 @@
-set PYTHONWARNINGS=ignore
-call C:\Miniconda3\Scripts\activate.bat C:\Miniconda3\envs\py3_pcse
-jupyter notebook
--- weather_generator.sh
+++
@@ -1,3 +0,0 @@
-cat head_weather_csv.txt>weather_full.csv
-cat moscow_full_weather.csv >> weather_full.csv
-cat tail_weather_csv.txt >> weather_full.csv
